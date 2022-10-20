a1cf5dbb9e9054848df1073179c607d13810232e
zcfrank1st <zhchaos@gmail.com>
2022-10-20 21:10:45 +0800

    change jinjia template

--- auto_dash.py
+++ auto_dash.py
@@ -48,14 +48,12 @@ app = Dash(__name__)
 
 header = html.H1(children='{{ info.title }}', style={'flex': 1})
 sub_header = html.Div(children='{{ info.sub_title }}', style={'flex': 1})
-
-{%- for source in sources -%}
+{% for source in sources -%}
 {%- set sindex = loop.index -%}
     {%- for skey in source.keys() %}
 df_{{sindex}} = {{ source[skey].data_handle }}
     {% endfor -%}
 {%- endfor -%}
-
 {%- for graph in graphs -%}
 {%- set gindex = loop.index -%}
     {%- for gkey in graph %}
@@ -63,11 +61,9 @@ fig_{{gindex}} = px.{{ gkey }}(df_{{gindex}}, **conf.get('graphs')[{{gindex - 1}
 graph_{{gindex}} = dcc.Graph(id='fig_{{gindex}}',figure=fig_{{gindex}})
     {% endfor -%}
 {%- endfor %}
-
 app.layout = html.Div(children=[
     header,
     sub_header,
-
 {% for n_row in range(layout.row) -%}
     {%- set graphs_list = [] -%}
     {%- for n_col in range(n_row * layout.column, (n_row+1) * layout.column) -%}
@@ -76,7 +72,6 @@ app.layout = html.Div(children=[
     {%- endfor %}
     html.Div(children={{ graphs_list | replace("'","") }}, style={'display': 'flex', 'flex-direction': 'row'}),
 {% endfor -%}
-
 ], style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center', 'justify-content': 'center'})
 
 if __name__ == '__main__':
